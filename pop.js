6c2a49a60fc13f087f1bcbba8ccd40c8b286191c
Jogendra Balotiya <2019jogendra.balotiya@ves.ac.in>
2022-08-21 20:26:29 +0530

    Add files via upload

---
+++ pop.js
@@ -0,0 +1,46 @@
+const btn=document.querySelector('.btn');
+const colorGrid=document.querySelector('.colorGrid');
+const colorValue=document.querySelector('.colorValue');
+
+
+btn.addEventListener('click',async()=>{
+    // console.log("clicked");
+
+    chrome.storage.sync.get('color', ({ color }) => {
+        console.log('color: ', color);
+    });
+
+    let[tab]=await chrome.tabs.query({active:true,currentWindow:true});
+    // console.log(tab);
+
+    chrome.scripting.executeScript(
+        {
+            target: { tabId: tab.id },
+            function: pickColor,
+        },
+        async (injectionResults) => {
+            const [data] = injectionResults;
+            if (data.result) {
+                const color = data.result.sRGBHex;
+                colorGrid.style.backgroundColor = color;
+                colorValue.innerText = color;
+                try {
+                    await navigator.clipboard.writeText(color);
+                } catch (err) {
+                    console.error(err);
+                }
+            }
+        }
+    );
+
+});
+
+async function pickColor(){
+    try {
+        
+        const eyeDropper = new EyeDropper();
+        return await eyeDropper.open();
+    } catch (err) {
+        console.error(err);
+    }
+}
